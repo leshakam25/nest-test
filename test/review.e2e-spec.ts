01c369f52addd810653dcf73ade582618aa8a0ec
leshakam25 <leshakam25@gmail.com>
2023-05-23 16:07:10 +0300

    init

--- test/review.e2e-spec.ts
+++ test/review.e2e-spec.ts
@@ -4,6 +4,7 @@ import {AppModule} from '../src/app.module';
 import {CreateReviewDto} from '../src/review/dto/create-review.dto';
 import {disconnect, Types} from 'mongoose';
 import * as request from 'supertest';
+import {REVIEW_NOT_FOUND} from '../src/review/review.constants';
 
 const productId = new Types.ObjectId().toHexString();
 
@@ -28,7 +29,7 @@ describe('AppController (e2e)', () => {
 		await app.init();
 	});
 
-	it('/review/create (POST)', async (done) => {
+	it('/review/create (POST) - success', async (done) => {
 		return request(app.getHttpServer())
 			.post('/review/create')
 			.send(testDto)
@@ -40,53 +41,53 @@ describe('AppController (e2e)', () => {
 			});
 	});
 
-	// it('/review/create (POST) - success', async (done) => {
-	// 	return request(app.getHttpServer())
-	// 		.post('/review/create')
-	// 		.send(testDto)
-	// 		.expect(201)
-	// 		.then(({ body }: request.Response) => {
-	// 			createdId = body._id;
-	// 			expect(createdId).toBeDefined();
-	// 			done();
-	// 		});
-	// });
-	//
-	// it('/review/byProduct/:productId (GET) - success', async (done) => {
-	// 	return request(app.getHttpServer())
-	// 		.get('/review/byProduct/' + productId)
-	// 		.expect(200)
-	// 		.then(({ body }: request.Response) => {
-	// 			expect(body.length).toBe(1);
-	// 			done();
-	// 		});
-	// });
-	//
-	// it('/review/byProduct/:productId (GET) - fail', async (done) => {
-	// 	return request(app.getHttpServer())
-	// 		.get('/review/byProduct/' + new Types.ObjectId().toHexString())
-	// 		.expect(200)
-	// 		.then(({ body }: request.Response) => {
-	// 			expect(body.length).toBe(0);
-	// 			done();
-	// 		});
-	// });
-	//
+	it('/review/create (POST) - fail', (done) => {
+		return request(app.getHttpServer())
+			.post('/review/create')
+			.send({...testDto, rating: 0})
+			.expect(400)
+			.then(({body}:request.Response) => {
+				console.log(body);
+				done();
+			});
+	});
+
+	it('/review/byProduct/:productId (GET) - success', async (done) => {
+		return request(app.getHttpServer())
+			.get('/review/byProduct/' + productId)
+			.expect(200)
+			.then(({body}: request.Response) => {
+				expect(body.length).toBe(1);
+				done();
+			});
+	});
+
+	it('/review/byProduct/:productId (GET) - fail', async (done) => {
+		return request(app.getHttpServer())
+			.get('/review/byProduct/' + new Types.ObjectId().toHexString())
+			.expect(200)
+			.then(({body}: request.Response) => {
+				expect(body.length).toBe(0);
+				done();
+			});
+	});
+
 	it('/review/:id (DELETE) - success', () => {
 		return request(app.getHttpServer())
 			.delete('/review/' + createdId)
 			.expect(200);
 	});
-	//
-	// it('/review/:id (DELETE) - fail', () => {
-	// 	return request(app.getHttpServer())
-	// 		.delete('/review/' + new Types.ObjectId().toHexString())
-	// 		.expect(404, {
-	// 			statusCode: 404,
-	// 			message: REVIEW_NOT_FOUND
-	// 		});
-	// });
-	//
+
+
+	it('/review/:id (DELETE) - fail', () => {
+		return request(app.getHttpServer())
+			.delete('/review/' + new Types.ObjectId().toHexString())
+			.expect(404, {
+				statusCode: 404,
+				message: REVIEW_NOT_FOUND
+			});
+	});
+
 	afterAll(() => {
 		disconnect();
 	});
