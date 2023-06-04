174a1490c9c248308e9fca4987217de5819af90f
Alexei <leshakam25@gmail.com>
2023-06-05 01:43:22 +0300

    work

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { ModelType, DocumentType } from '@typegoose/typegoose/lib/types';
+import {DocumentType, ModelType} from '@typegoose/typegoose/lib/types';
 import { Types } from 'mongoose';
 import { InjectModel } from 'nestjs-typegoose';
 import { CreateReviewDto } from './dto/create-review.dto';
