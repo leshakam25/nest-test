fa70ac04e93ff6f1d24bf15c3d1737c216464f76
Alexei <leshakam25@gmail.com>
2023-06-10 21:05:59 +0300

    init

---
+++ src/files/files.controller.ts
@@ -0,0 +1,16 @@
+import {Controller, HttpCode, Post, UploadedFile, UseGuards, UseInterceptors} from '@nestjs/common';
+import {FileInterceptor} from '@nestjs/platform-express';
+import {JwtAuthGuard} from '../auth/guards/jwt.guard';
+import {FileElementResponse} from './dto/file-element.response';
+
+@Controller('files')
+export class FilesController {
+
+	@Post('upload')
+	@HttpCode(200)
+	@UseGuards(JwtAuthGuard)
+	@UseInterceptors(FileInterceptor('files'))
+	async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<FileElementResponse[]> {
+
+	}
+}
